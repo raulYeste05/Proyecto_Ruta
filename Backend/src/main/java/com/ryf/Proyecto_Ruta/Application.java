281110fac1012fb6745802484044e65b23218e92
Raul Yeste <ryesfer0510@g.educaand.es>
2026-02-18 09:30:48 +0100

    Primer commit Backend

---
+++ Backend/src/main/java/com/ryf/Proyecto_Ruta/Application.java
@@ -0,0 +1,13 @@
+package com.ryf.Proyecto_Ruta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
