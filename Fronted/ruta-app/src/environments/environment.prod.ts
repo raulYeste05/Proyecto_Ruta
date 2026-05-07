006c0eaed75d0766475e43e5b9b92d23935b9eb0
raulYeste05 <146833276+raulYeste05@users.noreply.github.com>
2026-05-07 10:17:17 +0200

    Cambios en el environment.prod.ts

--- Fronted/ruta-app/src/environments/environment.prod.ts
+++ Fronted/ruta-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU-URL-DE-RAILWAY.up.railway.app', // <--- Cambiaremos esto pronto
-  orsApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4...'
+  apiUrl: 'https://proyectoruta-production.up.railway.app', 
+  orsApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImVjZWRhMmVjYjdmZTRiNzA5ODdhNzJmNDQzYzY0MmVmIiwiaCI6Im11cm11cjY0In0='
 };
