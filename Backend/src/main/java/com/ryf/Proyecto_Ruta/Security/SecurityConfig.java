1ef2d4c16b43b62c5eb519f5f00a4af0f13d8e79
raulYeste05 <146833276+raulYeste05@users.noreply.github.com>
2026-05-07 18:56:40 +0200

    creacion de User Admin

--- Backend/src/main/java/com/ryf/Proyecto_Ruta/Security/SecurityConfig.java
+++ Backend/src/main/java/com/ryf/Proyecto_Ruta/Security/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
